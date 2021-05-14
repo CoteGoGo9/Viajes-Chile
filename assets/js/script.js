cac32eb81c3863b13cfdb7e5156d81b25ec58419
Cote <cote.gonzalez9@gmail.com>
2021-05-14 00:44:33 -0400

    Comente un codigo de tooltip ya que no lo iba a usar al final

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,8 +14,8 @@ $(function(){
     }
   });
 
-    $('[data-toggle="tooltip"]').tooltip()
-    
+    // $('[data-toggle="tooltip"]').tooltip()
+
   // $('[data-toggle="popover"]').popover();
 
 });
